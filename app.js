012178841b4cd0ec14dc0d0dd903774440592ab1
harikach521 <dimpulchintala@gmail.com>
2021-11-03 15:32:41 -0500

    added addmods pug and js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var omletRouter = require('./routes/omlet');
+var addmodsRouter = require('./routes/addmods');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/omlet', omletRouter);
+app.use('/addmods', addmodsRouter);
 
 
 // catch 404 and forward to error handler
